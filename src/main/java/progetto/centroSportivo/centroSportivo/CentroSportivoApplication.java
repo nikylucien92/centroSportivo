fd6a0d747f95822a1494c9b6b0dd9aadd42f24ba
Nicola <n.pignatiello@itdm.it>
2026-07-10 20:39:28 +0200

    primo commit di importazione del progetto

---
+++ src/main/java/progetto/centroSportivo/centroSportivo/CentroSportivoApplication.java
@@ -0,0 +1,13 @@
+package progetto.centroSportivo.centroSportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroSportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroSportivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/progetto/centroSportivo/centroSportivo/CentroSportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package progetto.centroSportivo.centroSportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroSportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
